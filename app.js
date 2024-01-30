9c30fc2771a22b38721b716e8a2ffb69f96e800f
Jeremy Fowler <jembyfowler@gmail.com>
2024-01-30 11:43:11 -0700

    second break

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.groupCollapsed()
+
 // SECTION weird stuff
 console.log(2 + 2); // 4
 console.log('2' + '2'); //22
@@ -16,8 +18,8 @@ const person = {
 
 console.log(`
 Hi my name is ${person.name},
- I am ${person.age} years young,
-  and I ${person.lovesCats ? 'love' : 'hate'} cats.`);
+I am ${person.age} years young,
+and I ${person.lovesCats ? 'love' : 'hate'} cats.`);
 
 // !SECTION
 
@@ -37,9 +39,92 @@ for (let index = 0; index < strings.length; index++) {
   console.log(index);
   console.log(strings[index]);
 }
+
 // for (let index = 0; index < 10000; index++) {
 //   console.log('jerms is doin something');
 // }
 
 // !SECTION
 
+
+// SECTION array methods
+const cats = [
+  {
+    name: 'Gopher',
+    age: 3,
+    likesCheese: true,
+    favoriteToys: null
+  },
+  {
+    name: 'Georgie',
+    age: 8,
+    likesCheese: true,
+    favoriteToys: ['boxes', 'house plants']
+  },
+  {
+    name: 'Falcon',
+    age: 3,
+    likesCheese: false,
+    favoriteToys: ['laser pointer']
+  },
+  {
+    name: 'Iron Man',
+    age: 3,
+    likesCheese: true,
+    favoriteToys: ['laser pointer', 'string', 'mouse']
+  },
+]
+
+for (let i = 0; i < cats.length; i++) {
+  const cat = cats[i]
+  console.log(cat.name);
+}
+
+function sayHello() {
+  console.log('Hello!');
+}
+cats.forEach(sayHello)
+// cats.forEach((cat) => { console.log(cat.name) })
+cats.forEach(cat => console.log(cat.name))
+
+// const foundCat = cats.find(cat => cat.likesCheese == true) // NOTE will always pull out the first cat that likes cheese
+const foundCat = cats.find(cat => cat.name == 'Georgie') // NOTE pulls out the first cat whose name is georgie
+console.log('found a cat', foundCat);
+
+const filteredCats = cats.filter(cat => cat.likesCheese == false)
+console.log('filtered the cats', filteredCats);
+
+
+// !SECTION
+console.groupEnd()
+
+// ANCHOR application code starts here
+
+// NOTE global variable
+const menuItems = [
+  {
+    name: 'shrimp and steak scampi',
+    price: 15,
+    cartQuantity: 0
+  },
+  {
+    name: 'linguine with clams',
+    price: 23,
+    cartQuantity: 0
+  },
+  {
+    name: 'the jeanne fettuccine',
+    price: 18,
+    cartQuantity: 0
+  },
+]
+
+
+function addShrimpAndSteakScampiToCart() {
+  // pull correct object out of array
+  const shrimpAndSteakScampi = menuItems.find(menuItem => menuItem.name == 'shrimp and steak scampi')
+  console.log('got the scampi', shrimpAndSteakScampi);
+  // increase quantity
+  // shrimpAndSteakScampi.quantity++
+  // console.log(shrimpAndSteakScampi);
+}
